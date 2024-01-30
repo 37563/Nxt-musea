434e7fbcf32e5983a7e2c1e6c5ee8f7e118c67fc
Milan <37191@ma-web.nl>
2024-01-30 01:14:40 +0100

    Final_Product

--- web god/app.js
+++ web god/app.js
@@ -28,22 +28,18 @@
    let currentSlide = 0;
    const slides = document.getElementsByClassName('slide');
    function showSlide() {
-     // Hide all slides
+     
      for (let i = 0; i < slides.length; i++) {
        slides[i].style.display = 'none';
      }
-     // Show the current slide
+
      slides[currentSlide].style.display = 'block';
-     // Move to the next slide
+   
      currentSlide = (currentSlide + 1) % slides.length;
    }
-   // 1000 milliseconds (1 second)
    const intervalId = setInterval(showSlide, 10000);
    const maxIterations = 10;
    let iterations = 0;
-   /*function stopSlideshow() {
-     clearInterval(intervalId);
-   }
-    setTimeout(stopSlideshow, maxIterations * 3000);*/
+  
 
     
